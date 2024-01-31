7d428d5d7b37d1e3d64904a4430be8b166abc073
Diego Auza <auzajuan1@gmail.com>
2024-01-31 13:17:03 -0500

    adding images and css

--- src/page/banner/Banner.jsx
+++ src/page/banner/Banner.jsx
@@ -1,21 +1,29 @@
 import img from "../../assets/diego_Auza_Photo.webp";
 import git from "../../assets/Github_icon.webp"
 import link from "../../assets/LinkedIn_icon.webp"
+import backimg from "../../assets/backImage.webp";
 function Banner(){
+    const backgroundImageStyle = {
+      backgroundImage: `url(${backimg})`,
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      height: "80vh",
+      display : "flex",
+    };
     return (
-      <>
-        <section className="container">
-          <div className="container--banner">
-            <div className="banner--background">
-              <div className="banner--picture">
+      <div  style={backgroundImageStyle}>
+        <div className="banner--color">
+          <section className="container">
+            <div className="container--banner">
+              <div className="banner--background">
                 <img src={img} alt="Diego Auza picture" />
               </div>
               <div className="banner--content">
-                <div>
-                  <p>Diego Auza</p>
+                <div className="banner--text">
+                  <p>Hello, I am Diego Auza</p>
                   <h1>Mobile and Web developer</h1>
                 </div>
-                <ul className="banner--links">
+                <ul className="banner--links" role="list">
                   <li>
                     <a
                       href="https://www.linkedin.com/in/diego-auza-a64b1b253/"
@@ -35,9 +43,9 @@ function Banner(){
                 </ul>
               </div>
             </div>
-          </div>
-        </section>
-      </>
+          </section>
+        </div>
+      </div>
     );
 }
 export default Banner;
